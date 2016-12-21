2c9341b4851ce26f0c9027875511895913f73ad8
Nate Lehman <njlehman1@gmail.com>
2016-12-21 15:40:38 -0500

    fixed up the name and removed some non generic settings

--- init.el
+++ init.el
@@ -45,18 +45,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (leuven)))
- '(custom-safe-themes
-   (quote
-    ("cc60d17db31a53adf93ec6fad5a9cfff6e177664994a52346f81f62840fe8e23" default)))
  '(electric-indent-mode t)
  '(electric-pair-mode t)
  '(fringe-mode 6 nil (fringe))
  '(global-linum-mode t)
  '(linum-format (quote dynamic))
  '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Unifont" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
